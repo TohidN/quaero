2693cf69146dc9247d144cf12905ea22cac16f2d
Towhid <Towhidn@users.noreply.github.com>
2017-11-30 01:38:54 -0800

    crawl command: new parameter for external sites

--- project/management/commands/crawl-site.py
+++ project/management/commands/crawl-site.py
@@ -3,15 +3,17 @@ from quaero.crawler import Crawler
 
 
 class Command(BaseCommand):
-	help = 'create admin'
+	help = 'crawl site'
 
 	def add_arguments(self, parser):
 		parser.add_argument('url', nargs=1, type=str)
 		parser.add_argument('depth', nargs=1, type=int)
+		parser.add_argument('external', nargs=1, type=bool)
 
 	def handle(self, *args, **options):
 		url = options['url'][0]
-		depth =options['depth'][0]
+		depth = options['depth'][0]
+		external = options['external'][0]
 		print("Crawling: {}\nWith the depth: {}".format(url, depth))
-		crawler = Crawler(url=url, depth=depth)
+		crawler = Crawler(url=url, depth=depth, external=external)
 		pass
